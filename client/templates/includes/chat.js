68fa89dd472734443ef239eaf0e9723dad16ccd2
Henri Nybom <Henri.Nybom@solita.fi>
2015-10-08 07:36:41 +0300

    enter now trigger send

--- client/templates/includes/chat.js
+++ client/templates/includes/chat.js
@@ -1,3 +1,22 @@
+function sendMessage(message, template) {
+    var chat = {
+        message: message,
+        competitionId: template.data.competitionId
+    };
+
+    Meteor.call('insertNewMessage', chat, function (error, result) {
+        // display the error to the user and abort
+        if (error) {
+            throwError(error.reason)
+        } else {
+            $('#chat-btn-input').val('');
+            $("#chat-panel-body").each(function () {
+                var scrollHeight = Math.max(this.scrollHeight, this.clientHeight);
+                this.scrollTop = scrollHeight - this.clientHeight;
+            });
+        }
+    });
+}
 /**
  * Created by hnybom on 7.10.2015.
  */
@@ -7,27 +26,19 @@ Template.chat.events({
         $('#chat-button').toggle();
         $('#chat-box').slideToggle();
     },
+    'keypress #chat-btn-input': function(event, template) {
+        if (event.charCode == 13) {
+
+            var message = $('#chat-btn-input').val();
+            if(!message || message == "") return;
+            sendMessage(message, template);
+        }
+    },
     'click #chat-send-button': function(event, template) {
         var message = $('#chat-btn-input').val();
         if(!message || message == "") return;
 
-        var chat = {
-            message: message,
-            competitionId: template.data.competitionId
-        };
-
-        Meteor.call('insertNewMessage', chat, function(error, result) {
-            // display the error to the user and abort
-            if (error)
-                throwError(error.reason)
-
-        });
-
-        $('#chat-btn-input').val('');
-        $("#chat-panel-body").each( function() {
-            var scrollHeight = Math.max(this.scrollHeight, this.clientHeight);
-            this.scrollTop = scrollHeight - this.clientHeight;
-        });
+        sendMessage(message, template);
     }
 });
 
